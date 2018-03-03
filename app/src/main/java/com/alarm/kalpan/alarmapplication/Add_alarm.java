50231ae00f090a14d00ea7767d9b5afbb2c936a6
kalpanjasani <kjasani@purdue.edu>
2018-03-03 15:07:41 -0500

    I added a popup which would is a ringtone selector, in the screen of adding an alarm

--- app/src/main/java/com/alarm/kalpan/alarmapplication/Add_alarm.java
+++ app/src/main/java/com/alarm/kalpan/alarmapplication/Add_alarm.java
@@ -1,6 +1,7 @@
 package com.alarm.kalpan.alarmapplication;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -11,8 +12,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TimePicker;
+import android.media.RingtoneManager;
+
 
 import java.util.ArrayList;
+
+import static android.provider.Settings.System.DEFAULT_ALARM_ALERT_URI;
+import static android.provider.Settings.System.DEFAULT_NOTIFICATION_URI;
+import static android.provider.Settings.System.DEFAULT_RINGTONE_URI;
 
 public class Add_alarm extends AppCompatActivity {
 
@@ -51,9 +58,61 @@ public class Add_alarm extends AppCompatActivity {
             }
         });
 
+        /* next line makes an intent that will be used for startActivityForResult. That activity will be a popup */
+        Intent ringtonePickerIntent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
+        ringtonePickerIntent.putExtra(RingtoneManager.EXTRA_RINGTONE_SHOW_SILENT, false);   /* this basically is a mechanism for giving data to the activity where this intent would go. RingtoneManager defines all these constants and handles them approriately from the receiving end. This thing tells that we don't allow one option of silent in the list of sounds */
+        ringtonePickerIntent.putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE, RingtoneManager.TYPE_ALL);   /*important: we have all sounds, which I believe is more versatile and user friendly for the user. So even non alarm types would show up */
+        startActivityForResult(ringtonePickerIntent, 1);    /*reqeust code is 1 but is more helpful probably with many calls or complex programs, not for this testing purposes scenario */
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(resultCode == Activity.RESULT_CANCELED)  /* user backed out or operation failed for some reason */
+        {
+             /*
+             * we keep 1 as request code for the ringtone picker activity.
+             * There are many activities that we start, that can call this call back (the ones we start with startActivityForResult from this activity, will come here
+             */
+            if(requestCode == 1)
+            {
+                /*this case means that user pressed cancel on selecting the ringtone */
+                // handle this case
+            }
+        }
+
+        if(resultCode == Activity.RESULT_OK)
+        {
+            if(requestCode == 1)
+            {
+                Uri uri = data.getParcelableExtra(RingtoneManager.EXTRA_RINGTONE_PICKED_URI);   /* get the URI lying in the data of the intent parameter given in this function */
+
+                /* the uri above is the picked ringtone, or default and so on.
+                * the ringtone Manager class does all of that
+                * We have to use that but have to check its type, right ? (that is done below
+                 */
+
+                // nothing special comes to mind right now, but these default cases might require special consideration
+
+                if(uri.equals(DEFAULT_RINGTONE_URI))
+                {
+                    //handle
+                }
+                if(uri.equals(DEFAULT_ALARM_ALERT_URI))
+                {
+                    //handle
+                }
+                if(uri.equals(DEFAULT_NOTIFICATION_URI))
+                {
+                    //handle
+                }
+
+                // or else the ringtone is the selected ringtone
 
 
+                //important: we just need to save the URI right now, in the program, or in the memory. We don't need to play pause etc right now, right? (possibly right)
 
+            }
+        }
 
     }
 }
